5efcd630a246d27d624b4210c0d5fece6d429d93
zf <aaddggjj@foxmail.com>
2023-02-08 08:49:29 +0800

    first commit

---
+++ src/main/java/cn/tedu/csmall/passport/config/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package cn.tedu.csmall.passport.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    String[] url = new String[]{
+            "/doc.html",
+            "/**/*.js",
+            "/**/*.css",
+            "/swagger-resources",
+            "/v2/api-docs",
+            "/admins/login"
+    };
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().
+                mvcMatchers(url).
+                permitAll().
+                anyRequest().authenticated();
+        http.formLogin();
+    }
+}
